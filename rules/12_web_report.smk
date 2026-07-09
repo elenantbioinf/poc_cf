8e9086843e72532062961db0ca763f80eaa014c6
Elena Núñez <elena.nt.bioinf@gmail.com>
2026-07-10 01:52:03 +0200

    Add coverage evaluability plot to web report

--- rules/12_web_report.smk
+++ rules/12_web_report.smk
@@ -6,7 +6,8 @@ rule r12_01_render_rmd:
     input:
         script = "scripts/render_web_report.R",
         rmd = config["12_web_report"]["rmd_template"],
-        final_report_json = "final_report/{id}.final_report.json"
+        final_report_json = "final_report/{id}.final_report.json",
+        coverage_plot = config["07_coverage"]["out_dir"] + "/{id}.target_region_evaluability.png"
     output:
         html = config["12_web_report"]["out_dir"] + "/{id}.web_report.html"
     log:
@@ -19,5 +20,6 @@ rule r12_01_render_rmd:
             {input.rmd} \
             {output.html} \
             {input.final_report_json} \
+            {input.coverage_plot} \
             > {log} 2>&1
         """
--- scripts/render_web_report.R
+++ scripts/render_web_report.R
@@ -7,17 +7,54 @@
 library(rmarkdown)
 
 args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args) != 4) {
+  stop(
+    paste0(
+      "Expected 4 arguments: rmd_file, out_html, final_report_json, coverage_plot. ",
+      "Received ", length(args), " arguments: ",
+      paste(args, collapse = " | ")
+    )
+  )
+}
+
 rmd_file <- args[1]
 out_html <- args[2]
 final_report_json <- args[3]
+coverage_plot <- args[4]
 
-dir.create(dirname(out_html), recursive = TRUE, showWarnings = FALSE)
+out_dir <- dirname(out_html)
+out_file <- basename(out_html)
+
+dir.create(out_dir, recursive = TRUE, showWarnings = FALSE)
+
+coverage_plot_report <- file.path(
+  out_dir,
+  basename(coverage_plot)
+)
+
+copy_ok <- file.copy(
+  from = coverage_plot,
+  to = coverage_plot_report,
+  overwrite = TRUE
+)
+
+if (!copy_ok) {
+  stop("Coverage plot could not be copied to the final report directory.")
+}
+
+final_report_json_abs <- normalizePath(final_report_json, mustWork = TRUE)
+coverage_plot_abs <- normalizePath(coverage_plot_report, mustWork = TRUE)
 
 rmarkdown::render(
   input = rmd_file,
-  output_file = basename(out_html),
-  output_dir = dirname(out_html),
-  params = list(final_report_json = normalizePath(final_report_json)),
+  output_file = out_file,
+  output_dir = out_dir,
+  params = list(
+    final_report_json = final_report_json_abs,
+    target_region_evaluability_plot = coverage_plot_abs
+  ),
   knit_root_dir = getwd(),
+  envir = new.env(),
   quiet = TRUE
 )
